94a8fdf22b892413e41a9517580f50a7a34ac069
gaurav-mishra-genzeon <gaurav.mishra@genzeon.com>
2024-02-27 19:17:43 +0530

    Day 1 code

---
+++ Day1/first.py
@@ -0,0 +1,35 @@
+print('Hello world')
+
+#Reserved words
+# False, class,return,is,finally,None, if, for, lamda,continue,True,def,from,while,nonlocal,and,del,global,not,with,as,elif,try,or,
+# yield,assert,else,import,pass,break,expect,in,raise
+
+x=2
+x=x+2
+print(x)
+
+print(2/2)
+
+
+# Data Types 
+
+#int for whole numbers 3 300, 
+# float for numbers with decimal point 2.3 4.6, 100.0, 
+# str for strings "hello world"
+# list- ordered sequence of objects  [10,"hello", 200.3]
+# dictionaries- dict Unordered Key:Value pairs: {'myKey':"value","name":"Frankie"}
+#Tuples- tup-  Ordered immutable sequence of objects: (10,"hello", 200.3)
+# Sets unordered collection of unique objects {"a","b"}
+#booleans true or false
+
+
+print(50+50)
+print(150-50)
+print(10*10)
+print(1000/10)
+
+#print(0.1+0.2-0.3) will not be equal to 0 due to  floating-point representation issues
+  
+
+
+
---
+++ Day1/string.py
@@ -0,0 +1,61 @@
+
+my_dogs=2
+
+my_dogs=["Sammy","Frankie"]
+
+a=10
+print(a)
+a=a+a
+print(a)
+print(type(a))
+
+a=30.1
+print(type(a))
+
+my_income= 100
+tax_rate= 0.2
+my_taxes= my_income*tax_rate
+
+print(my_taxes)
+
+
+c='this is a string'
+#white space count as a charatcer in above string
+
+print('hello\nworld') #for a new line
+
+print(len(c))  # length of 
+
+
+#Indexing
+mystring='Hello World'
+print(mystring[0])  # H
+
+print(mystring[-1]) # d 
+
+
+#Slicing strings
+
+str="Hello World"
+#    012345678910
+print(str[:3])  # Hel
+print(str[3:])  #lo World 
+print(str[2:7]) #llo W
+print(str[1:6:2])  #el
+print(str[::-1])  #dlroW olleH
+
+#Immutability 
+name="Sam"
+# name[0]='P' 
+# print(name)#'str' object does not support item assignment
+
+
+#str
+last_letters=name[2:]
+
+print("S"+ last_letters)  
+
+
+
+
+
